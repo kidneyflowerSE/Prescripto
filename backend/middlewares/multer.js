f3ee60532814830947ddd39b086ee30dd5c52d25
kidneyflowerSE <ilovehtmlandcss@gmail.com>
2025-03-16 21:03:43 +0700

    fix navbar(add mobile responsive, add my profile, my appointment, fix doctor(filter)

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+    filename: function(req, file, callback) {
+        callback(null, file.originalname)
+    }
+})
+
+
+const upload = multer({
+    storage
+})
+
+
+export default upload
